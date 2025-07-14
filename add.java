2b8754f1f81c84b0c2ac930640a1fe2db797d128
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2025-07-14 17:02:34 +0530

    java programs

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add{
+    public static void main(String[] args){
+        //variable
+        int a = 25;
+        int b = 40;
+        int sum = a + b;
+        System.out.println(sum);
+        //int diff = a - b;
+        //System.out.println(diff);
+        //int product = a * b;
+        //System.out.println(product);
+    }
+}
