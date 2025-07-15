00cf516f52eb45538fede85b14893ccc1da5c7ec
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2025-07-15 12:51:50 +0530

    todays coding

---
+++ Condition_switch.java
@@ -0,0 +1,19 @@
+//import java.util.*;
+
+public class Condition_switch{
+    public static void main(String[] args){
+        //Scanner sc = new Scanner(System.in);
+        //int button = sc.nextInt();
+        int button = 2; // Example button, you can change this value to test different cases
+        switch(button) {
+            case 1: System.out.println("Hello");
+                    break;
+            case 2: System.out.println("Namaste");
+                    break;  
+            case 3: System.out.println("vanakkam");
+                    break;
+            default: System.out.println("Invalid");
+        }
+     
+        }
+ }
