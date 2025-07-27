14f2694418f72ce99a20e31b3ffae7bffa372e70
Bhavana Manjunath <117250531+bhavanagowda04@users.noreply.github.com>
2025-07-27 20:07:18 +0530

    Update Abstract.java

--- Abstract.java
+++ Abstract.java
@@ -3,29 +3,30 @@ abstract class Animal { // abstract is juzt concept it cannot be instantiated or
     Animal() {
         System.out.println("You are creating an animal");
     }
-  
 }
 
 class Horse extends Animal {
     Horse() {
+        super(); // Added for clarity; calls Animal constructor
         System.out.println("created a horse");
     }
-    public void walk(){
+    
+    public void walk() {
         System.out.println("walks on 4 legs");
     }
 }
 
 class Chicken extends Animal {
-    public void walk(){
+    public void walk() {
         System.out.println("walks on 2 legs");
     }
 }
 
-public class Abstract {
-    public static void main(String args[]){
+public class AbstractClassDemo { // public class Abstract {
+    // public class Abstract {
+    public static void main(String args[]) {
         Horse horse = new Horse();
         horse.walk();
-       // horse.eats();
-
+        // horse.eats(); // This line is commented out because 'eats()' method does not exist
     }
 }
