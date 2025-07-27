1c072cd19c7bbe55b089b80edb950e898ddad2db
Bhavana Manjunath <117250531+bhavanagowda04@users.noreply.github.com>
2025-07-27 20:08:50 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,27 +1,31 @@
+// File name: Polymorphism.java
+
 class Student_poly {
     String name;
     int age;
     String rollNo;
 
     public void printInfo(String name) {
-        System.out.println("Name: " + this.name);
-    }
-    public void printInfo(int age) {
-        System.out.println("Age: " + this.age);
-    }
-    public void printInfo(String name, int age) {
-        System.out.println("Name is" + this.name + " and age is " + this.age);
+        System.out.println("Name: " + name);
     }
 
-   
+    public void printInfo(int age) {
+        System.out.println("Age: " + age);
+    }
+
+    public void printInfo(String name, int age) {
+        System.out.println("Name is " + name + " and age is " + age);
+    }
 }
 
-public class Polymorphism{
-    public static void main(String args[]) {
-        Student s1 = new Student();
+public class Polymorphism {
+    public static void main(String[] args) {
+        Student_poly s1 = new Student_poly();
         s1.name = "Bhavana";
         s1.age = 20;
-        s1.printInfo(s1.age); 
-    }
 
+        s1.printInfo(s1.name);
+        s1.printInfo(s1.age);
+        s1.printInfo(s1.name, s1.age);
+    }
 }
