25d00455431d0840e9eb5e656f314ecd18e0d05a
Bhavana Manjunath <117250531+bhavanagowda04@users.noreply.github.com>
2025-07-27 19:38:33 +0530

    Delete src directory

--- src/main/java/mathproblem.java
+++
@@ -1,9 +0,0 @@
-public class mathproblem {
-    public static void main(String[] args){
-        int a = 10;
-        int b = 5;
-        int calculate = (a * b) / (a - b);
-        // Every operation in java goes from left to right
-        System.out.println(calculate);
-    }
-}
