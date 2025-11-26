724e6d0ee57f2e0ac056ea0ebf88a8e9b35a0615
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2025-11-26 13:09:18 +0530

    day9 practice questions

--- src/com/practice04/FlippingImage.java
+++ src/com/practice04/FlippingImage.java
@@ -1,4 +1,29 @@
 package com.practice04;
 
+import java.util.Arrays;
+
 public class FlippingImage {
+    public static void main(String[] args) {
+        int[][] image = {
+                {1,1,0},
+                {1,0,1},
+                {0,0,0}
+        };
+        System.out.println(Arrays.toString(flipAndInvertImage(image)));
+    }
+
+    static int[][] flipAndInvertImage(int[][] image) {
+//        int r = image.length;
+//        int c = image[0].length;
+        int[][] arr = new int[image.length][image[0].length];
+        for(int r = 0; r < image.length; r++){
+//            for(int c = 0; c < image[0].length; c--){
+                while(r != image.length-1){
+                    int[] c = {image[0].length % 10};
+                    return new int[][]{};
+                }
+            }
+//        }
+        return new int[][] {};
+    }
 }
---
+++ src/com/practice04/RotateArray.java
@@ -0,0 +1,37 @@
+package com.practice04;
+//https://leetcode.com/problems/rotate-array/description/
+
+import java.util.Arrays;
+
+class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        if (n <= 1) return;
+        k = k % n;
+        if (k == 0) return;
+
+        // reverse whole array
+        reverse(nums, 0, n - 1);
+        // reverse first k elements
+        reverse(nums, 0, k - 1);
+        // reverse remaining n-k elements
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] a, int i, int j) {
+        while (i < j) {
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            i++; j--;
+        }
+    }
+
+    // test helper
+    public static void main(String[] args) {
+        RotateArray s = new RotateArray();
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        s.rotate(nums, 5);
+        System.out.println(Arrays.toString(nums)); // [5,6,7,1,2,3,4]
+    }
+}
