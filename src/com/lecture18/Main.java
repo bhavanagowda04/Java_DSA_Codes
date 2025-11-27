ee2d6468fb2544b799ba4f4ab96fb2bd40dda3e2
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2025-11-27 22:30:23 +0530

    day10 dsa

---
+++ src/com/lecture18/Main.java
@@ -0,0 +1,39 @@
+package com.lecture18;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,1,2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndes(arr, 0, last);
+
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxIndes(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+}
+
+
