d5ed7126101bf5cddbe15abc5691d9965e580541
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2025-12-28 19:36:28 +0530

    CycleSort LeetCode5

---
+++ src/com/lecture20/SetMismatch.java
@@ -0,0 +1,35 @@
+package com.lecture20;
+//https://leetcode.com/problems/set-mismatch/
+
+import java.util.Arrays;
+
+class SetMismatch {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,4};
+        System.out.println(findErrorNums(nums));
+    }
+    static int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while(i< nums.length) {
+            int correct = nums[i] -1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            } else{
+                i++;
+            }
+        }
+
+        for(int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return new int[]{nums[index],index + 1};
+
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
