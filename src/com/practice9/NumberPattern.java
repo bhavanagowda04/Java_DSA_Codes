b2948d6b705a8e36b1279ad50fca193b6cc90117
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2025-12-31 16:21:35 +0530

    patters done

--- src/com/practice9/NumberPattern.java
+++ src/com/practice9/NumberPattern.java
@@ -5,6 +5,7 @@ public class NumberPattern {
         pattern1(5);
         pattern30(5);
         pattern17(4);
+        pattern31(4);
     }
 
     static void pattern1(int n){
@@ -50,11 +51,12 @@ public class NumberPattern {
     }
 
     static void pattern31(int n){
+        int originalN = n;
         n = 2 * n;
-        for(int row = 0; row <= 2 * n -1 ; row++ ){
-            for(int col = 0; col <= 2 * n -1; col++){
-                int atEveryIndex =
-                System.out.println(atEveryIndex);
+        for(int row = 0; row <= n ; row++ ){
+            for(int col = 0; col <= n; col++){
+                int atEveryIndex =originalN - Math.min(Math.min(row, col),Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
             }
             System.out.println();
         }
