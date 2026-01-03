8518c5e461a052856c655ba645c8f893850917d4
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-03 20:19:32 +0530

    leetcode

--- src/com/lecture23/FibonacciNum.java
+++ src/com/lecture23/FibonacciNum.java
@@ -1,4 +1,16 @@
 package com.lecture23;
 
 public class FibonacciNum {
+    static void main() {
+        System.out.println(fibo(6));
+
+    }
+
+    static int fibo(int n){
+        if(n < 2){
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
 }
---
+++ src/com/practice08/IPaddress.java
@@ -0,0 +1,13 @@
+package com.practice08;
+//https://leetcode.com/problems/defanging-an-ip-address/description/
+import java.util.Arrays;
+
+public class IPaddress {
+    static void main() {
+        String address =  "1.1.1.1";
+        System.out.println(defangIPaddr(address));
+    }
+
+    static String defangIPaddr(String address) {
+       return address.replace("." , "[.]");
+    }
