d6cb4d0e327395fd9b3791bf7b93fa83a0b844ce
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-04 12:28:40 +0530

    recursion day 2

---
+++ src/com/lecture23/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.lecture23;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int target = 3;
+        System.out.println(bs(arr, target,0, arr.length -1));
+    }
+
+    static int bs(int[] arr, int target, int s, int e){
+       if (s > e){
+           return -1;
+       }
+       int m = s + (e -s) /2;
+
+       if(arr[m] == target){
+           return m;
+       }
+       if(target < arr[m]){
+           return bs(arr, target, s, m-1);
+       }
+       return bs(arr, target, m + 1, e);
+    }
+
+}
