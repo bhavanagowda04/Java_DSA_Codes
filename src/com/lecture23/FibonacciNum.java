1c938c6dec31a23c2c01f7df2e91f3a50a9de942
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-05 12:57:13 +0530

    space and time complexity

--- src/com/lecture23/FibonacciNum.java
+++ src/com/lecture23/FibonacciNum.java
@@ -2,15 +2,24 @@ package com.lecture23;
 
 public class FibonacciNum {
     static void main() {
-        System.out.println(fibo(6));
-
-    }
-
-    static int fibo(int n){
-        if(n < 2){
-            return n;
+//        System.out.println(fibo(6));
+        for ( int i = 0; i < 11; i++) {
+            System.out.println(fiboformula(i));
         }
 
-        return fibo(n-1) + fibo(n-2);
+        System.out.println(fiboformula(50));
     }
+
+    static int fiboformula(int n){
+        return (int)(Math.pow(((1  + Math.sqrt(5)) / 2),n) / Math.sqrt(5));
+
+    }
+
+//    static int fibo(int n){
+//        if(n < 2){
+//            return n;
+//        }
+//
+//        return fibo(n-1) + fibo(n-2);
+//    }
 }
