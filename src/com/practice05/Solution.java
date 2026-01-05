3699f3272cd749a0b7cfba7a0a27582dff5faa2b
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-05 16:12:54 +0530

    leetcode

---
+++ src/com/practice05/Solution.java
@@ -0,0 +1,25 @@
+package com.practice05;
+
+class Solution {
+    static void main() {
+        String sentence = "bcdefghijklmnopqrstuvwxyz";
+        System.out.println(checkIfPangram(sentence));;
+    }
+    static boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26){
+            return false;
+        }
+        for(char ch = 'a'; ch <= 'z'; ch++ ){
+            boolean ans = false;
+            for (int i = 0; i < sentence.length(); i++) {
+                if(ch == sentence.charAt(i)){
+                    ans = true;
+                }
+            }
+            if(ans == false){
+                return false;
+            }
+        }
+        return true;
+    }
+}
