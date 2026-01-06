9372712fb89f57655160ef8ce19bdcc262073fb0
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-06 12:13:11 +0530

    bitwise Problems 4

---
+++ src/com/lecture25/NoOfDigits.java
@@ -0,0 +1,12 @@
+package com.lecture25;
+
+public class NoOfDigits {
+    static void main(String[] args) {
+        int n = 34567;
+        int b = 10;
+
+        int ans =  (int)(Math.log(n) / Math.log(b) + 1);
+
+        System.out.println(ans);
+    }
+}
