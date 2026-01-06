327ec89a7962b8f5ded7ee343de2af5705eb35fd
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-06 12:08:35 +0530

    bitwise Problems 3

---
+++ src/com/lecture25/MagicNums.java
@@ -0,0 +1,17 @@
+package com.lecture25;
+
+public class MagicNums {
+    static void main(String[] args) {
+        int n = 6;
+
+        int ans = 0;
+        int base = 5;
+        while(n > 0){
+            int last = n & 1;
+            n = n >> 1;
+            ans += last * base;
+            base = base * 5;
+        }
+        System.out.println(ans);
+    }
+}
