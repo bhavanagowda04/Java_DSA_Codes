ec0c0392e3c4cd5caed725de9f8e704eb8690e4c
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-06 15:38:52 +0530

    leetcode 69

---
+++ src/com/practice05/Solution.java
@@ -0,0 +1,13 @@
+package com.practice05;
+//https://leetcode.com/problems/sqrtx/description/
+class Solution {
+    static void main(String[] args) {
+        int x = 8;
+        System.out.println(mySqrt(x));
+    }
+    static int mySqrt(int x) {
+        int a = (int) Math.sqrt(x);
+        return a;
+        
+    }
+}
