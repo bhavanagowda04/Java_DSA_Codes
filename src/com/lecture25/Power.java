1c88845093b6c8aa81b229c7c43488bdce40bab1
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-06 12:32:19 +0530

    bitwise Problems 6

---
+++ src/com/lecture25/Power.java
@@ -0,0 +1,19 @@
+package com.lecture25;
+
+public class Power {
+    static void main(String[] args) {
+        int base =3;
+        int power = 6;
+
+         int ans = 1;
+
+         while(power > 0){
+             if ((power & 1) == 1){
+                  ans *= base;
+             }
+             base *= base;
+             power = power >> 1;
+         }
+        System.out.println(ans);
+    }
+}
