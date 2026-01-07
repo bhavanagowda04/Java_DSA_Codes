f82e795f3d7ad9e5096e76c465ac10a40626c044
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-07 20:31:58 +0530

    leetcode 167

--- src/com/practice05/SqrtOfNum.java
+++ src/com/practice05/SqrtOfNum.java
@@ -1,6 +1,6 @@
 package com.practice05;
 //https://leetcode.com/problems/sqrtx/description/
-class Solution {
+class SqrtOfNum {
     static void main(String[] args) {
         int x = 8;
         System.out.println(mySqrt(x));
---
+++ src/com/practice05/TwoSum2.java
@@ -0,0 +1,23 @@
+package com.practice05;
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+import java.util.Arrays;
+
+public class TwoSum2 {
+    static void main(String[] args) {
+        int[] numbers = {2,3,4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+
+    static int[] twoSum(int[] numbers, int target) {
+        for(int i = 0; i < numbers.length; i++ ){
+            for(int j = i + 1; j < numbers.length; j++ ){
+                if(numbers[i] + numbers[j] == target ){
+                    return new int[] {i + 1 , j + 1};
+                }
+            }
+
+        }
+        return new int[] { -1, -1};
+    }
+}
