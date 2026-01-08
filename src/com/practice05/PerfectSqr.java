c0e320f621a55e028438725669789f7816db973e
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-08 11:42:41 +0530

    Math for DSA 4

--- src/com/practice05/PerfectSqr.java
+++ src/com/practice05/PerfectSqr.java
@@ -2,16 +2,24 @@ package com.practice05;
 
 public class PerfectSqr {
     static void main(String[] args) {
-        int num = 16;
+        int num = 14;
         System.out.println(isPerfectSquare(num));
     }
 
     static boolean isPerfectSquare(int num) {
-        int ans = (int) Math.sqrt(num);
-        if( ans == Integer.parseInt(String.valueOf(num))){
-            return true;
-        }else {
-            return false;
+        int s = 0;
+        int e = num;
+        while( s <= e){
+            int m = s + (e - s)/ 2;
+            if(m * m == num){
+                return true;
+            }
+            if(m * m > num){
+                e = m - 1;
+            } else{
+                s = m + 1;
+            }
         }
+        return false;
     }
 }
