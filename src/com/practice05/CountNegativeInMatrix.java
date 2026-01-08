368347e64d97651a379985b1b71ebb8c91c1cb7b
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-08 13:34:15 +0530

    leetcode 1351

--- src/com/practice05/CountNegativeInMatrix.java
+++ src/com/practice05/CountNegativeInMatrix.java
@@ -1,9 +1,10 @@
 package com.practice05;
+//https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
 
 public class CountNegativeInMatrix {
     public static void main(String[] args) {
         int[][] grid = {
-                {4,3,2,-1},
+                {-5,2,-1},
                 {3,2,1,-1},
                 {1,1,-1,-2},
                 {-1,-1,-2,-3}
@@ -13,6 +14,14 @@ public class CountNegativeInMatrix {
 
     static int countNegatives(int[][] grid) {
         int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] < 0 ){
+                    count++;
+                }
+            }
+
+        }
 
         return count;
     }
