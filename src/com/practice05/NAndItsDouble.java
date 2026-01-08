0dd4688c6120249138172822d0fc55b1cf0da539
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-08 13:28:03 +0530

    leetcode 1346

---
+++ src/com/practice05/NAndItsDouble.java
@@ -0,0 +1,21 @@
+package com.practice05;
+//https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+
+public class NAndItsDouble {
+    static void main(String[] args) {
+        int[] arr = {-20,8,-6,4};
+        System.out.println(checkIfExist(arr));
+    }
+    static boolean checkIfExist(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            int j = 0;
+                while (j < arr.length) {
+                    if (i!= j && arr[i] == 2 * arr[j]) {
+                        return true;
+                    }
+                    j++;
+                }
+        }
+        return false;
+    }
+}
