21b8c90ae082b8dfc78daf4a0f97a1579f2072b4
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-09 09:46:47 +0530

    leetcode 125

--- src/com/practice08/PalidromeString1.java
+++ src/com/practice08/PalidromeString1.java
@@ -1,18 +1,30 @@
 package com.practice08;
+//https://leetcode.com/problems/valid-palindrome/description/
 
 class PalidromeString1 {
     static void main(String[] args) {
-        String s = "A man, a plan, a canal: Panama";
+        String s = " ";
         System.out.println(isPalindrome(s));
     }
     static boolean isPalindrome(String s) {
-        if(s.length() == 0 ){
+        StringBuilder cleaned = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                cleaned.append(ch);
+            }
+        }
+
+        String n = cleaned.toString();
+
+        if(n.length() == 0 ){
             return true;
         }
-        s = s.toLowerCase();
-        for(int i = 0; i <= s.length()/2; i++){
-            char start = s.charAt(i);
-            char end = s.charAt(s.length() -1 -i);
+        n = n.toLowerCase();
+
+        for(int i = 0; i <= n.length()/2; i++){
+            char start = n.charAt(i);
+            char end = n.charAt(n.length() -1 -i);
             if(start != end) {
                 return false;
             }
