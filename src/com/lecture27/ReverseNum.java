8a1dba10e4d507afcb24bc0fe53c218ef9a308df
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-09 10:54:55 +0530

    Recursion basic code practice 6

---
+++ src/com/lecture27/ReverseNum.java
@@ -0,0 +1,33 @@
+package com.lecture27;
+
+public class ReverseNum {
+    static void main(String[] args) {
+        rev(1234);
+        System.out.println(ans);
+        System.out.println(rev2(12345));
+    }
+
+    static int ans = 0;
+    static void rev(int n){
+        if(n == 0){
+            return;
+        }
+        int rem = n % 10;
+        ans = ans * 10 + rem;
+        rev(n / 10);
+    }
+
+    static int rev2(int n){
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n,digits);
+    }
+
+    private static int helper(int n, int digits){
+        if( n % 10 ==  n){
+            return n;
+        }
+        int rem= n % 10;
+        return rem * (int)Math.pow(10, digits -1) + helper(n/10, digits -1);
+
+    }
+}
