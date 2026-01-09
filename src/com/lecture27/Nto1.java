db774fd57d268b02a267bfe185a5e17cfaf2262c
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-09 10:15:36 +0530

    Recursion basic code practice 2

--- src/com/lecture27/Nto1.java
+++ src/com/lecture27/Nto1.java
@@ -3,13 +3,32 @@ package com.lecture27;
 public class Nto1 {
     static void main(String[] args) {
         fun(5);
+        funrev(5);
+        funboth(5);
     }
 
     static void fun(int n){
         if(n == 0){
             return;
         }
+        System.out.print(n + " "); //prints 54321
+        fun(n - 1);
+    }
+
+    static void funrev(int n){
+        if(n == 0){
+            return;
+        }
+        funrev(n - 1);
+        System.out.print(n + " "); // prints 12345
+    }
+
+    static void funboth(int n){
+        if(n == 0){
+            return;
+        }
         System.out.print(n + " ");
-        fun(n-1);
+        funboth(n - 1);
+        System.out.print(n + " ");
     }
 }
