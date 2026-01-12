a1572568919867bb2d39372ba432f9b4825676f1
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-12 09:55:48 +0530

    leetcode 441

---
+++ src/com/practice05/ArrangingCoins.java
@@ -0,0 +1,20 @@
+package com.practice05;
+//https://leetcode.com/problems/arranging-coins/description/
+
+class ArrangingCoins {
+    static void main(String[] args) {
+        System.out.println(arrangeCoins(3));
+    }
+    static int arrangeCoins(int n) {
+        int row = 1;
+        int count = 0;
+
+        while(n >= row){
+            n = n - row;
+            count++;
+            row++;
+        }
+        return count;
+
+    }
+}
