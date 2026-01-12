29c032f079ec39de2bbf723b70b8ba8b10fb684c
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-12 11:58:15 +0530

    Recursion Subset Duplicates

--- src/com/lecture32/SubsetArray.java
+++ src/com/lecture32/SubsetArray.java
@@ -1,13 +1,14 @@
 package com.lecture32;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //Time Complexity = O(N * 2^n)
 public class SubsetArray {
     static void main(String[] args) {
-        int[] arr = {1,2,3};
-        List<List<Integer>> ans = subset(arr);
+        int[] arr = {1,2,2};
+        List<List<Integer>> ans = subsetDuplicates(arr);
         for(List<Integer> list : ans){
             System.out.println(list);
         }
@@ -24,6 +25,31 @@ public class SubsetArray {
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
                 outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    static List<List<Integer>> subsetDuplicates(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+        int start =0;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            if(i> 0 && arr[i] == arr[i-1]){
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+
             }
         }
         return outer;
