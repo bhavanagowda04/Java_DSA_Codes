7d7c8f05473f89c956261ab224a7f96b321e0252
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-12 13:48:02 +0530

    leetcode 17

--- src/com/lecture34/LetterCombination_17.java
+++ src/com/lecture34/LetterCombination_17.java
@@ -1,22 +1,24 @@
 package com.lecture34;
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombination_17 {
     static void main(String[] args) {
-        pad("","12");
+        pad("", "12");
         System.out.println(pad1("", "12"));
     }
 
     static void pad(String p, String up) {
-        if(up.isEmpty()){
+        if (up.isEmpty()) {
             System.out.println(p);
             return;
         }
         int digit = up.charAt(0) - '0';   //this will convert '2' to 2
         for (int i = (digit - 1) * 3; i < digit * 3; i++) {
-            char ch = (char)('a' + i);
+            char ch = (char) ('a' + i);
 
             pad(p + ch, up.substring(1));
         }
@@ -25,7 +27,7 @@ public class LetterCombination_17 {
 
 
     static ArrayList<String> pad1(String p, String up) {
-        if(up.isEmpty()){
+        if (up.isEmpty()) {
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
@@ -34,26 +36,47 @@ public class LetterCombination_17 {
         ArrayList<String> list = new ArrayList<>();
 
         for (int i = (digit - 1) * 3; i < digit * 3; i++) {
-            char ch = (char)('a' + i);
+            char ch = (char) ('a' + i);
             list.addAll(pad1(p + ch, up.substring(1)));
         }
         return list;
     }
 
-    static List<String> letterCombinations(String digits) {
-        String p = "";
-        if(digits.isEmpty()){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.isEmpty()) {
+            return result;
         }
-        int digit = digits.charAt(0) - '0';   //this will convert '2' to 2
-        ArrayList<String> list = new ArrayList<>();
 
-        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
-            char ch = (char)('a' + i);
-            list.addAll(pad1(p + ch, digits.substring(1)));
+        helper("", digits, result);
+        return result;
+    }
+
+    static void helper(String p, String up, List<String> result) {
+        if (up.isEmpty()) {
+            result.add(p);
+            return;
         }
-        return list;
+
+        int digit = up.charAt(0) - '0';
+
+        String[] map = {
+                "",     // 0
+                "",     // 1
+                "abc",  // 2
+                "def",  // 3
+                "ghi",  // 4
+                "jkl",  // 5
+                "mno",  // 6
+                "pqrs", // 7
+                "tuv",  // 8
+                "wxyz"  // 9
+        };
+
+        String letters = map[digit];
+
+        for (int i = 0; i < letters.length(); i++) {
+            helper(p + letters.charAt(i), up.substring(1), result);
+        }
     }
 }
