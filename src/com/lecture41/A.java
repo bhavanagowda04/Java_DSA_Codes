a7070658d2e1722c8ebab4c524d3ae5a6eac7386
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-19 11:13:59 +0530

    NestedInterface

---
+++ src/com/lecture41/A.java
@@ -0,0 +1,16 @@
+package com.lecture41;
+
+public class A {
+    public interface NestedInterface {
+        boolean isOdd(int num);
+    }
+}
+
+class B implements A.NestedInterface{
+
+    @Override
+    public boolean isOdd(int num) {
+        return (num & 1) == 1;
+    }
+}
+
---
+++ src/com/lecture41/Main.java
@@ -0,0 +1,8 @@
+package com.lecture41;
+
+public class Main {
+    static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.isOdd(5));
+    }
+}
