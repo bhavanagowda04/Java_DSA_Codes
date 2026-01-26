41ef521c903cd14f3cc2093d97a3b33a8846a8e4
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-26 21:18:12 +0530

    Leetcode 136

--- src/com/Leetcodes/FindUniqueNum.java
+++ src/com/Leetcodes/FindUniqueNum.java
@@ -1,4 +1,6 @@
-package com.lecture25;
+package com.Leetcodes;
+//https://leetcode.com/problems/single-number/description/?envType=problem-list-v2&envId=array
+
 public class FindUniqueNum {
     static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 2, 3, 4, 6, 1};
