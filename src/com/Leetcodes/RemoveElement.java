2ce3a322d7c9d3ef47790669f2e0fd8885861bba
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-01-31 22:33:56 +0530

    Leetcode 27

---
+++ src/com/Leetcodes/RemoveElement.java
@@ -0,0 +1,23 @@
+package com.Leetcodes;
+
+//https://leetcode.com/problems/remove-element/
+
+class RemoveElement {
+    static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        System.out.println(removeElement(nums,val));
+
+    }
+    static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
