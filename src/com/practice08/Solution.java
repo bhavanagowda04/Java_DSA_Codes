ebf270caf40052fc91f6377d31cbb24aac58b21d
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-02-14 23:02:37 +0530

    Leetcode 557

---
+++ src/com/practice08/Solution.java
@@ -0,0 +1,20 @@
+package com.practice08;
+//https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder temp = new StringBuilder(words[i]);
+            result.append(temp.reverse());
+            
+            if (i != words.length - 1) {
+                result.append(" ");
+            }
+        }
+        
+        return result.toString();
+    }
+}
