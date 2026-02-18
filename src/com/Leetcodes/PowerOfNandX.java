ff27d5eb7a66a8eba0e033e9d8cec8da8c911ffa
bhavanagowda04 <bhavanamanjunath04@gmail.com>
2026-02-18 22:13:09 +0530

    Leetcode 50

---
+++ src/com/Leetcodes/PowerOfNandX.java
@@ -0,0 +1,8 @@
+package com.Leetcodes;
+//https://leetcode.com/problems/powx-n/description/
+
+class PowerOfNandX {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+    }
+}
